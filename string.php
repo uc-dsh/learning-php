bf0c9a3db6b902098c22683fab26db65d86b0d8c
uc-dsh <dilshad.ahmed@ucertify.com>
2023-07-26 17:19:35 +0530

    string commit

---
+++ string.php
@@ -0,0 +1,15 @@
+<?php
+// Declare var and Assign string val to it.
+$string = "hello world";
+
+// concat 2 strings
+$concat = "string1" . "string2";
+
+// using string as array.
+$string[0] = strtoupper($string[0]);
+
+echo $string , $concat;
+
+// ways to print a string
+// echo $string;
+// print $string
