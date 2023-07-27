a7931a0c419989b34731d0bae2920d9bb0e187a5
uc-dsh <dilshad.ahmed@ucertify.com>
2023-07-27 17:54:39 +0530

    Include And Require

---
+++ include-require.php
@@ -0,0 +1,13 @@
+<?php
+// include("./index.php");
+// require("./index.php");
+
+
+// include_once("./index.php");
+// require_once("./index.php");
+
+include("./include-test.php");
+$incVar++;
+echo $incVar, "<br />";
+include("./include-test.php");
+echo $incVar;
