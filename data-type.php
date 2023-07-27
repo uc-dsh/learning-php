a516b8e8974e9404770fbd8a50ca656885ef1fc2
uc-dsh <dilshad.ahmed@ucertify.com>
2023-07-27 11:20:16 +0530

    DataTypes

---
+++ data-type.php
@@ -0,0 +1,18 @@
+<?php
+## Scalar type
+// int
+$int = 1;
+
+// string
+$string = "Hello World";
+
+// bool
+$bool = true;
+
+// float 
+$float = 1.1;
+
+
+## Compound Type
+// array
+$array = ["Hello", "World"];
